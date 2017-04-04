85c6018e860685e3ad596c6274044bb5b907eee2
Georgi Pasquali <gpasquali@shopmetrics.com>
2017-04-04 17:58:41 +0300

    initial commit( not much done.)

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path");
+
+module.exports = {
+  /*
+   * app.ts represents the entry point to your web application. 
+   * Everything included in this app.ts gets compiled and bundled 
+   * into a single js bundle. Acts like a 'main' file.
+   */
+  entry: ["./src/app.ts"],
+
+  // In the output we specify where to store the bundle and under what filename.
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+
+  /*
+   * The resolve lets you specify the filetypes that you are going to use in your app.
+   */
+  resolve: {
+    extensions: ["*", ".ts", ".tsx", ".js"]  // " " extension is deprecated
+  },
+
+  module: {
+    /*
+     * Goes trough all files and checks trough regex if they
+     * need to be loaded and if so it uses the loader specified.
+     */
+    loaders: [
+      { // this is the ts loader which uses the tsc compiler trough the tsconfig.
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
