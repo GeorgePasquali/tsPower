ac08ab053d7641a9627db7bdc46c8119c3a2dd33
Georgi Pasquali <gpasquali@shopmetrics.com>
2017-04-05 17:58:50 +0300

    secondary commit (tried some basic class division)

---
+++ src/_constructionPattern.ts
@@ -0,0 +1,10 @@
+import * as selector from 'd3-selection';
+
+export default class constructionPattern{
+    private parentSelector: any;
+
+
+    public setParentSelector(selector: any){
+        this.parentSelector = selector;
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-console.log("mohammed ali");
+import User from './someModule';
+import constructionPattern from './_constructionPattern'
+import * as selector from 'd3-selection';
+import * as axis from 'd3-axis';
+
+class HDC {
+    private _properties:any;
+    private _data: any;
+    private _constructionPattern: constructionPattern;
+
+
+
+    constructor(divContainer:string, chartType: string, properties: object){
+        
+        if(divContainer)
+        {
+            let setParentSelector = selector.select(divContainer);
+
+            this._constructionPattern.setParentSelector(setParentSelector);
+        }
+
+    }
+
+
+
+
+}
+
+var goga = new User('goga', 22);
+goga.introduceYourself();
+goga.useD3();
+
+selector.select("body").append("rect").text("this is my first d3 inside my env");
+
+
